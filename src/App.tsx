4ce1a763ad0b95bfc11540b29cf5e5a7a1fd5748
ezio815 <exio78@hotmail.com>
2024-08-16 23:02:06 -0300

    Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Hero from "./components/Hero/Hero.tsx";
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Hero />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={ <Hero /> } />
+      </Routes>
     </>
   )
 }
